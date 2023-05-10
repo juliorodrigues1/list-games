7ff793ef2de0249fd435babe81b5e219c229d193
Júlio Cesar <juliorodrigues.ac@gmail.com>
2023-05-09 20:43:54 -0500

    criando construtor para converter um GameMinProjection para GameMinDTO

--- src/main/java/com/listgames/listgames/dto/GameMinDTO.java
+++ src/main/java/com/listgames/listgames/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.listgames.listgames.dto;
 
 import com.listgames.listgames.entities.Game;
+import com.listgames.listgames.projections.GameMinProjection;
 
 public class GameMinDTO {
     private Long id;
@@ -17,6 +18,13 @@ public class GameMinDTO {
         this.year = entity.getYear();
         this.imgUrl = entity.getImgUrl();
         this.shortDescription = entity.getShortDescription();
+    }
+    public GameMinDTO(GameMinProjection projection) {
+        this.id = projection.getId();
+        this.title = projection.getTitle();
+        this.year = projection.getYear();
+        this.imgUrl = projection.getImgUrl();
+        this.shortDescription = projection.getShortDescription();
     }
 
     public Long getId() {
