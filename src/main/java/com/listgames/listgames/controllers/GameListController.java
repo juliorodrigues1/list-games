a1998f657fc458f2d60c7513b1cee5d6fbc1aa8c
Júlio Cesar <juliorodrigues.ac@gmail.com>
2023-05-09 20:21:15 -0500

    criando GameListController

---
+++ src/main/java/com/listgames/listgames/controllers/GameListController.java
@@ -0,0 +1,26 @@
+package com.listgames.listgames.controllers;
+
+
+import com.listgames.listgames.dto.GameListDTO;
+import com.listgames.listgames.services.GameListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "lists")
+public class GameListController {
+
+    @Autowired
+    GameListService gameListService;
+
+    @GetMapping
+    public List<GameListDTO> findAll(){
+        return gameListService.findAll();
+    }
+
+
+}
