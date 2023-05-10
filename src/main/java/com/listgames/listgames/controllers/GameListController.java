60db309b79fa05bf6c3bf1efa9b889748066b264
Júlio Cesar <juliorodrigues.ac@gmail.com>
2023-05-09 20:44:43 -0500

    utilizando metodo de busca por lista

--- src/main/java/com/listgames/listgames/controllers/GameListController.java
+++ src/main/java/com/listgames/listgames/controllers/GameListController.java
@@ -2,9 +2,12 @@ package com.listgames.listgames.controllers;
 
 
 import com.listgames.listgames.dto.GameListDTO;
+import com.listgames.listgames.dto.GameMinDTO;
 import com.listgames.listgames.services.GameListService;
+import com.listgames.listgames.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +20,17 @@ public class GameListController {
     @Autowired
     GameListService gameListService;
 
+    @Autowired
+    GameService gameService;
+
     @GetMapping
     public List<GameListDTO> findAll(){
         return gameListService.findAll();
+    }
+
+    @GetMapping(value = "{listId}/game")
+    public List<GameMinDTO> findByList(@PathVariable Long listId){
+        return gameService.findByList(listId);
     }
 
 
