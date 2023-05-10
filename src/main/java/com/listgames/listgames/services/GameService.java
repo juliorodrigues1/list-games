540b0705b2ec6bc821b2029e5352ede954fe4e2e
Júlio Cesar <juliorodrigues.ac@gmail.com>
2023-05-09 20:08:14 -0500

    criando metodo de buscar por id

--- src/main/java/com/listgames/listgames/services/GameService.java
+++ src/main/java/com/listgames/listgames/services/GameService.java
@@ -1,10 +1,12 @@
 package com.listgames.listgames.services;
 
+import com.listgames.listgames.dto.GameDto;
 import com.listgames.listgames.dto.GameMinDTO;
 import com.listgames.listgames.entities.Game;
 import com.listgames.listgames.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,9 +16,16 @@ public class GameService {
     @Autowired
     GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         List<Game> games = gameRepository.findAll();
         List<GameMinDTO> dto = games.stream().map(x -> new GameMinDTO(x)).toList();
         return dto;
+    }
+
+    @Transactional(readOnly = true)
+    public GameDto findById(Long id){
+        var game = gameRepository.findById(id).get();
+        return new GameDto(game);
     }
 }
