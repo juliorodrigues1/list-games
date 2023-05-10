5a6aede20d7a0ef197374a6eaf7fac4ed9f02a27
Júlio Cesar <juliorodrigues.ac@gmail.com>
2023-05-09 20:08:27 -0500

    utilizando metodo de busca por id

--- src/main/java/com/listgames/listgames/controllers/GameController.java
+++ src/main/java/com/listgames/listgames/controllers/GameController.java
@@ -1,9 +1,11 @@
 package com.listgames.listgames.controllers;
 
+import com.listgames.listgames.dto.GameDto;
 import com.listgames.listgames.dto.GameMinDTO;
 import com.listgames.listgames.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,9 +18,13 @@ public class GameController {
     @Autowired
     GameService gameService;
 
-
     @GetMapping
     public List<GameMinDTO> findAll(){
         return gameService.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public GameDto findById(@PathVariable Long id){
+        return gameService.findById(id);
     }
 }
