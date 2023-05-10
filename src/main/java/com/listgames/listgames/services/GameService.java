ceed59c408e7a72719e3048eb964a3a68a309cd4
Júlio Cesar <juliorodrigues.ac@gmail.com>
2023-05-09 20:44:34 -0500

    criando metodo de busca por lista

--- src/main/java/com/listgames/listgames/services/GameService.java
+++ src/main/java/com/listgames/listgames/services/GameService.java
@@ -3,6 +3,7 @@ package com.listgames.listgames.services;
 import com.listgames.listgames.dto.GameDto;
 import com.listgames.listgames.dto.GameMinDTO;
 import com.listgames.listgames.entities.Game;
+import com.listgames.listgames.projections.GameMinProjection;
 import com.listgames.listgames.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -27,5 +28,11 @@ public class GameService {
     public GameDto findById(Long id){
         var game = gameRepository.findById(id).get();
         return new GameDto(game);
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId){
+        List<GameMinProjection> games = gameRepository.searchByList(listId);
+        return  games.stream().map(x -> new GameMinDTO(x)).toList();
     }
 }
